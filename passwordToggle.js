e1945a312b89cb7fc7122653e64e1eca405f1d17
Muhammad Ismail <you@example.com>
2025-03-27 15:46:40 +0500

    added landing page

--- passwordToggle.js
+++ passwordToggle.js
@@ -9,7 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
           this.classList.toggle('fa-eye-slash');
       });
 
-    // Handle form submission
     loginForm.addEventListener('submit', (event) => {
       event.preventDefault();
       const username = document.getElementById('username').value;
@@ -18,10 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log('Username:', username);
       console.log('Password:', password);
 
-      // Add login logic here
     });
 
 
-    // Initialize with the visible SVG
     togglePassword.innerHTML = visibleSVG;
   });
